510704a1e1fb5190d860698bb0a5b0715018466f
marialaurasc311 <marialaurasc311@gmail.com>
2020-09-18 19:17:14 -0400

    Con controlador Suarez + autorizacion

---
+++ suarez/suarez/Controllers/SuarezsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using suarez.Models;
+
+namespace suarez.Controllers
+{
+    public class SuarezsController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/Suarezs
+        [Authorize]
+        public IQueryable<Suarez> GetSuarezs()
+        {
+            return db.Suarezs;
+        }
+
+        // GET: api/Suarezs/5
+        [Authorize]
+        [ResponseType(typeof(Suarez))]
+        public IHttpActionResult GetSuarez(int id)
+        {
+            Suarez suarez = db.Suarezs.Find(id);
+            if (suarez == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(suarez);
+        }
+
+        // PUT: api/Suarezs/5
+        [Authorize]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSuarez(int id, Suarez suarez)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != suarez.suarezID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(suarez).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SuarezExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Suarezs
+        [Authorize]
+        [ResponseType(typeof(Suarez))]
+        public IHttpActionResult PostSuarez(Suarez suarez)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Suarezs.Add(suarez);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = suarez.suarezID }, suarez);
+        }
+
+        // DELETE: api/Suarezs/5
+        [Authorize]
+        [ResponseType(typeof(Suarez))]
+        public IHttpActionResult DeleteSuarez(int id)
+        {
+            Suarez suarez = db.Suarezs.Find(id);
+            if (suarez == null)
+            {
+                return NotFound();
+            }
+
+            db.Suarezs.Remove(suarez);
+            db.SaveChanges();
+
+            return Ok(suarez);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SuarezExists(int id)
+        {
+            return db.Suarezs.Count(e => e.suarezID == id) > 0;
+        }
+    }
+}
---
+++ suarez/suarez/Models/Suarez.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace suarez.Models
+{
+    public enum placeType
+    {
+        micasa,
+        ventura,
+        lasbrisas,
+        burtown,
+        laplaza
+    }
+    public class Suarez
+    {
+        [Key]
+        public int suarezID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre completo")]
+        [StringLength(24,MinimumLength =2)]
+        public string Friendofsuarez { get; set; }
+
+        [Required]
+        public placeType Place { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name ="Cumpleanos")]
+        [DisplayFormat(DataFormatString ="{0:dd,MM,yyyy}")]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
+    }
+}
